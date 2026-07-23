bfff18236a69d89ffa18853bc7a84e41dda76654
Numair25 <mohammednumair3978@gmail.com>
2026-07-23 13:14:47 +0530

    Remove temp sync scripts

--- check.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once( dirname(__DIR__, 3) . '/wp-load.php' );
-global $wpdb;
-$table_attendees = $wpdb->prefix . 'emp_attendees';
-$count = $wpdb->get_var("SELECT COUNT(id) FROM $table_attendees WHERE phone IS NOT NULL AND phone != ''");
-file_put_contents('count.txt', $count);
